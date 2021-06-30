b9ead1b187870f3874c0f422f3be9644ab0ed925
Mohib Khan <mohibk.in@gmail.com>
2021-06-30 16:56:19 +0530

    style: navbar height

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,10 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <header className="bg-maroon text-white">
-      <nav className="flex justify-between h-10 items-center max-w-screen-lg mx-auto">
-        <Link to="/" className="p-4 text-lg">
+      <nav className="flex justify-between h-12 items-center max-w-screen-lg mx-auto">
+        <Link to="/" className="p-4 font-semibold text-lg">
           Notework
         </Link>
 
