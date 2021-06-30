a4ae4c2c718eddc219ffc14551ea321cfb66189d
Mohib Khan <mohibk.in@gmail.com>
2021-06-30 16:02:43 +0530

    style: notebook edit & delete icon

--- src/pages/notes.js
+++ src/pages/notes.js
@@ -46,11 +46,40 @@ export default function Notes() {
 
   return (
     <>
-      <div className="max-w-screen-lg mx-auto mt-8">
-        <span className="flex space-x-3">
-          <h2>{notebook.notebookName}</h2>
-          <button onClick={() => setEditNotebook(true)}>Edit</button>
-          <button onClick={() => setDeleteNotebookModal(true)}>Delete</button>
+      <div className="max-w-screen-lg mx-auto mt-8 px-3 mb-2">
+        <span className="flex items-center space-x-3">
+          <h2 className="font-bold text-3xl">{notebook.notebookName}</h2>
+
+          <svg
+            onClick={() => setEditNotebook(true)}
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 cursor-pointer"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
+            />
+          </svg>
+          <svg
+            onClick={() => setDeleteNotebookModal(true)}
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 cursor-pointer"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
         </span>
 
         <EditNotebook
