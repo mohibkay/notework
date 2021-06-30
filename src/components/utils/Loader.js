d97a868f445afbfc0f3d1a3a975c3b8a0746a0a8
Mohib Khan <mohibk.in@gmail.com>
2021-06-30 16:52:55 +0530

    new: add loader

---
+++ src/components/utils/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <p>Loading...</p>
+    </div>
+  );
+}
+
+export default Loader;
